1dcd00364ebb5f3fcff6ad9e7d8393b404061df0
bot000001 <103040206+bot000001@users.noreply.github.com>
2022-04-05 16:03:13 +0300

    Add files via upload

---
+++ Lab4/second/Router.rb
@@ -0,0 +1,39 @@
+require_relative "CommentsController"
+require_relative "PostsController"
+class Router
+  def initialize
+    @routes = {}
+  end
+
+  def init
+    resources(PostsController, 'posts')
+    resources(CommentsController, 'comments')
+
+    loop do
+      print 'Choose resource you want to interact (1 - Posts, 2 - Comments, q - Exit): '
+      choise = gets.chomp
+
+      PostsController.connection(@routes['posts']) if choise == '1'
+
+      CommentsController.connection(@routes['comments']) if choise == '2'
+      if choise == 'q'
+        puts 'Good bye!'
+        break
+      end
+    end
+  end
+
+  def resources(klass, keyword)
+    controller = klass.new
+    @routes[keyword] = {
+      'GET' => {
+        'index' => controller.method(:index),
+        'show' => controller.method(:show)
+      },
+      'POST' => controller.method(:create),
+      'PUT' => controller.method(:update),
+      'DELETE' => controller.method(:destroy)
+    }
+  end
+end
+
